e713ff8eb10b5608826b11e5c3c2ab921f83e676
yuto1115 <yutown1115@gmail.com>
2020-08-02 00:08:29 +0900

    updated main

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import "github.com/yuto1115/othello/tools"
+import (
+	"fmt"
+	"github.com/yuto1115/othello/tools"
+	"os"
+	"strconv"
+)
 
 func main() {
-	b := tools.NewBoard()
-	b.Display()
+	board := tools.NewBoard()
+	for {
+		board.Display()
+		fmt.Println("Enter two numbers '[row number][column number]'")
+
+	LABEL:
+		var s string
+		fmt.Scanf("%s", &s)
+
+		if s == "exit" {
+			os.Exit(0)
+		}
+		if len(s) != 2 {
+			fmt.Println("invalid input; please try again")
+			goto LABEL
+		}
+
+		i, e1 := strconv.Atoi(s[0:1])
+		if e1 != nil {
+			fmt.Println("invalid input; please try again")
+			goto LABEL
+		}
+
+		j, e2 := strconv.Atoi(s[1:2])
+		if e2 != nil {
+			fmt.Println("invalid input; please try again")
+			goto LABEL
+		}
+
+		pos := tools.Position{I: i - 1, J: j - 1}
+		if pos.OutOfRange() {
+			fmt.Println("invalid input; please try again")
+			goto LABEL
+		}
+
+		err := board.Place(pos)
+		if err != nil {
+			fmt.Println("You can't place your piece there; please try again")
+			goto LABEL
+		}
+	}
 }
